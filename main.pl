f618bb76af45d9b9d4bbfd8ad5175a444a5a3950
Gayuh Tri Rahutami <69070763+wundersmith@users.noreply.github.com>
2020-11-21 23:10:41 +0700

    nambahin level up sm status.

--- main.pl
+++ main.pl
@@ -1,3 +1,5 @@
+:- include('player.pl').
+
 openGame :-
     write('Let\'s play and be a programmer\n'),
     write('%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%\n'),
@@ -11,48 +13,53 @@ openGame :-
     write('% 7. a      : gerak ke barat 1 langkah                                         %\n'),
     write('% 9. Status : menampilkan status pemain                                        %\n'),
     write('% 8. help   : menampilkan segala bantuan                                       %\n'),
-    write('%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%\n').
+    write('%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%').
 
 startGame :-
     write('Welcome to Genshin Asik. Choose your job\n'),
-    write('   1. Web Developer\n'),
-    write('   2. ML Engineer\n'),
-    write('   3. Mobile Developer'),
+    write('1. Web Developer\n'),
+    write('2. ML Engineer\n'),
+    write('3. Mobile Developer\n'),
     read(Job),
     welcomePlayer(Job),
-    initPlayer(Job). 
+    initPlayer(Job).
 
 /*MAP
 Disimpan dalam bentuk list of lists.
 Diprint secara rekursif -> Basisnya ketika tail kosong.
 */
+/*
 map :-
     printMap(FileMap).
 
-printMap(FileMap). /*Fungsi rekursif*/
+printMap(FileMap). *//*Fungsi rekursif*/
 
 /* INVENTORY */
 /* Disimpan dalam bentuk list */
+/*
 inventory :-
-    /*gatau tapi ngambil data dari database*/,
+    gatau tapi ngambil data dari database,
     writeInvetory(DataInventory).
 
 writeInvetory. /*Fungsi rekursif, Basis ketika Tail = 0*/
 
+
 /* EXPLORATION */
 /* generateEnemy adalah fungsi yang menentukan apakah akan ada enemy yang muncul atau tidak ketika player pindah tempat */
+/*
 w :-
-/* Y = Y - 1*/,
+Y = Y - 1,
 generateEnemy().
 
 a :-
-/* X = X - 1*/,
+X = X - 1,
 generateEnemy().
 
 s :-
-/* Y = Y + 1*/,
+ Y = Y + 1,
 generateEnemy().
 
 d :-
-/* X = X + 1*/,
-generateEnemy(). 
+ X = X + 1,
+generateEnemy().
+*/
