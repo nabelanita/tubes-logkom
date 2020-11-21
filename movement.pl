e029118125e801354154c96b4e8e0bb8d94ac2e5
nabelanita <utami.nabelanita@gmail.com>
2020-11-21 15:22:01 +0700

    movement fix

--- movement.pl
+++ movement.pl
@@ -8,14 +8,14 @@ s :- down, !.
 d :- right, !.
 
 /* change player position */
-up :- player(_,Y), Y =:= 0, write('You just hit a wall!\n'), nl, !.
-up :- retract(playerPos(X,Y)), Y > 0, Y1 is Y-1, nl, asserta(playerPos(X,Y1)),!.
+up :- player(_,Y), Y =:= 1, write('You just hit a wall!\n'), nl, !.
+up :- retract(playerPos(X,Y)), Y > 1, Y1 is Y-1, nl, asserta(playerPos(X,Y1)),!.
 
-down :- player(_,Y), Y =:= 11, write('You just hit a wall!\n'), nl, !.
-down :- retract(playerPos(X,Y)), Y < 11, Y1 is Y+1, nl, asserta(playerPos(X,Y1)),!.
+down :- player(_,Y), Y =:= 10, write('You just hit a wall!\n'), nl, !.
+down :- retract(playerPos(X,Y)), Y < 10, Y1 is Y+1, nl, asserta(playerPos(X,Y1)),!.
 
-right :- player(X,_), X =:= 0, write('You just hit a wall!\n'),nl,!.
-right :- retract(playerPos(X,Y)), X > 0, X1 is X+1, nl, asserta(playerPos(X1,Y)),!.
+right :- player(X,_), X =:= 20, write('You just hit a wall!\n'),nl,!.
+right :- retract(playerPos(X,Y)), X < 20, X1 is X+1, nl, asserta(playerPos(X1,Y)),!.
 
-left :- player(X,_), X =:= 20, write('You just hit a wall!'), nl,!.
+left :- player(X,_), X =:= 1, write('You just hit a wall!'), nl,!.
 left :- retract(playerPos(X,Y)), X > 1, X1 is X-1, nl, asserta(playerPos(X1,Y)),!.
