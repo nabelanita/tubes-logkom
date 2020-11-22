f1b3e7f2a966e64601c237a0c60280fc7d08cf4b
nabelanita <utami.nabelanita@gmail.com>
2020-11-22 11:44:32 +0700

    add item info

---
+++ item.pl
@@ -0,0 +1,42 @@
+/* PS. KALO ADA YG PERLU DIUBAH, UBAH AJA : D */
+
+
+/* includes */
+
+
+/* Dynamic variables */
+
+/* Weapons */
+/* weapon(name, harga, damage) */
+weapon(keyboard, 100, 50).
+weapon(code_editor, 125, 75).
+weapon(laptop, 200, 100).
+weapon(pc, 300, 150).
+weapon(super_computer, 450, 200).
+
+/* Armor */
+/* armor(name, harga, defense). */
+armor(headphones, 75, 25).
+armor(wifi, 125, 50).
+armor(database, 175, 75).
+armor(server, 250, 125).
+armor(ai, 350, 175).
+
+/* Accessory */
+/* Accessory target job */
+accWebdev(figma).
+accWebdev(framework).
+
+accMobDev(simulator).
+accMobDev(phone).
+
+accML(voice_recog).
+accML(robot).
+
+/* acc(name, price, damage+, defense+) */
+accessory(figma, 100, 50, 25).
+accessory(framework, 150, 75, 50).
+accessory(simulator, 100, 50, 25).
+accessory(phone, 150, 75, 50).
+accessory(voice_recog, 100, 25).
+accessory(robot, 150, 50).
