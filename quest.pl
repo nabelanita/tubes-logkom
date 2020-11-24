8cc57e01a957eb8765ab01430c35e27950e53476
auliaadila <auliaadila36@gmail.com>
2020-11-24 10:39:39 +0700

    update quest tp blm bisa ambil playerPos dari player.pl

--- quest.pl
+++ quest.pl
@@ -1,17 +1,16 @@
 %:- include('player.pl').
-:- dynamic(quest/4).
+:- dynamic(quest/3).
 %:- dynamic
 
 /* inisialisi quest scr random, hrs menyelesaikan : 
    W buah milestone
    X buah maintenance
-   Y buah data breach
-   Z buah work stolen */
-/*quest(W,X,Y,Z).*/
+   Y buah data breach */
+/*quest(W,X,Y).*/
 playerPos(2,1).
 
 initQuest :-
-    asserta(quest(0,0,0,0)),!.
+    asserta(quest(0,0,0)),!.
 
 isInQ :-
     playerPos(2,1).
@@ -20,28 +19,22 @@ isInQ :-
     random(1,3,LvlPlayer).*/
 
 addMile(W) :-
-    quest(Wawal,X,Y,Z),
-    retract(quest(Wawal,X,Y,Z)),
+    quest(Wawal,X,Y),
+    retract(quest(Wawal,X,Y)),
     Wakhir is W + Wawal,
-    asserta(quest(Wakhir,X,Y,Z)),!.
+    asserta(quest(Wakhir,X,Y)),!.
 
 addMaint(X) :-
-    quest(W,Xawal,Y,Z),
-    retract(quest(W,Xawal,Y,Z)),
+    quest(W,Xawal,Y),
+    retract(quest(W,Xawal,Y)),
     Xakhir is X + Xawal,
-    asserta(quest(W,Xakhir,Y,Z)),!.
+    asserta(quest(W,Xakhir,Y)),!.
 
 addData(Y) :-
-    quest(W,X,Yawal,Z),
-    retract(quest(W,X,Yawal,Z)),
+    quest(W,X,Yawal),
+    retract(quest(W,X,Yawal)),
     Yakhir is Y + Yawal,
-    asserta(quest(W,X,Yakhir,Z)),!.
-
-addWork(Z) :-
-    quest(W,X,Y,Zawal),
-    retract(quest(W,X,Y,Zawal)),
-    Zakhir is Z + Zawal,
-    asserta(quest(W,X,Y,Zakhir)),!.
+    asserta(quest(W,X,Yakhir)),!.
 
 /*Jika player tidak berada dalam pos quest*/
 questLevel(LvlPlayer) :-
@@ -53,8 +46,7 @@ questLevel(LvlPlayer) :-
     initQuest,
     random(1,2,W), addMile(W),
     random(1,2,X), addMaint(X),
-    addData(1),
-    addWork(1).
+    addData(1).
 
 questLevel(LvlPlayer) :-
     isInQ,
@@ -62,8 +54,7 @@ questLevel(LvlPlayer) :-
     initQuest,
     random(1,3,W), addMile(W),
     random(1,3,X), addMaint(X),
-    random(1,2,Y), addData(Y),
-    random(1,2,Z), addWork(Z).
+    random(1,2,Y), addData(Y).
 
 questLevel(LvlPlayer) :-
     isInQ,
@@ -73,16 +64,15 @@ questLevel(LvlPlayer) :-
     B is LvlPlayer + 3,
     random(A,B,W), addMile(W),
     random(A,B,X), addMaint(X),
-    random(A,B,Y), addData(Y),
-    random(A,B,Z), addWork(Z).
+    random(A,B,Y), addData(Y).
 
 isEmptyQuest :- 
-    quest(0,0,0,0),
+    quest(0,0,0),
     write('You don\'t have any task to solve!\n'),!.
 
 printQuest :-
     \+ isEmptyQuest,
-    quest(W,X,Y,Z),
+    quest(W,X,Y),
     write('\n[  Your task  ]\n'),
-    format('Milestone  : ~d\nMaintenance: ~d\nData Breach: ~d\nWork Stolen: ~d\n', [W,X,Y,Z]).
+    format('Milestone  : ~d\nMaintenance: ~d\nData Breach: ~d\n', [W,X,Y]).
 
