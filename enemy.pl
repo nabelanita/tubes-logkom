1d9e77a4a4168b1ff42dff034beebe7bca93fa60
Gayuh Tri Rahutami <69070763+wundersmith@users.noreply.github.com>
2020-11-26 04:37:00 +0700

    battle udh bener harusnya amin

--- enemy.pl
+++ enemy.pl
@@ -19,45 +19,48 @@ attackMenu :-
     enemyStatus,
     write('What\'s your next move?\n').
 
-playerDamaged(Damage) :- Damage < 1, !.
+%belom game over
 
-playerDamaged(Damage) :-
-    Damage > 0,
+ checkPlayerDefeated(1) :-
     player(Role, Lvl, Exp, Attack, Defense, MaxHP, HP, Hearts, Gold),
-    NewHP is HP - Damage,
-    NewHP > 0,
-    retract(player(Role, Lvl, Exp, Attack, Defense, MaxHP, HP, Hearts, Gold)),
-    asserta(player(Role, Lvl, Exp, Attack, Defense, MaxHP, NewHP, Hearts, Gold)), !.
+    HP > 0, !.
 
-playerDamaged(Damage) :-
-    Damage > 0,
+ checkPlayerDefeated(0) :-
     player(Role, Lvl, Exp, Attack, Defense, MaxHP, HP, Hearts, Gold),
-    NewHP is HP - Damage,
-    NewHP < 1,
+    HP =< 0,
+    NewHearts is Hearts - 1,
+    NewHearts > 0, 
+    write('You were defeated!\n'),
     retract(player(Role, Lvl, Exp, Attack, Defense, MaxHP, HP, Hearts, Gold)),
     retract(inBattle(1)), asserta(inBattle(0)), 
     retract(specialAttackCount(_)),
     retract(enemySpecialAttack(_)), 
-    write('You were defeated!\n'),
-    NewHearts is Hearts - 1,
-    NewHearts > 0, 
     format('Current hearts: ~d\n', [NewHearts]),
     asserta(player(Role, Lvl, Exp, Attack, Defense, MaxHP, MaxHP, NewHearts, Gold)), !.
 
-playerDamaged(Damage) :-
-    Damage > 0,
+%game over
+%keknya nanti bikin fungsi game over aja buat ngeretract smua database
+checkPlayerDefeated(0) :-
     player(Role, Lvl, Exp, Attack, Defense, MaxHP, HP, Hearts, Gold),
-    NewHP is HP - Damage,
-    NewHP < 1,
+    HP =< 0,
+    NewHearts is Hearts - 1,
+    NewHearts =< 0, 
+    write('You were defeated!\n'),
     retract(player(Role, Lvl, Exp, Attack, Defense, MaxHP, HP, Hearts, Gold)),
     retract(inBattle(1)), asserta(inBattle(0)), 
     retract(specialAttackCount(_)),
     retract(enemySpecialAttack(_)), 
-    write('You were defeated!\n'),
-    NewHearts is Hearts - 1,
-    NewHearts =:= 0, 
     format('Current hearts: ~d\n', [NewHearts]),
     write('You got fired!'), !.
+
+playerDamaged(Damage, 0) :- Damage < 1, !.
+
+playerDamaged(Damage, Damage) :-
+    Damage > 0,
+    player(Role, Lvl, Exp, Attack, Defense, MaxHP, HP, Hearts, Gold),
+    NewHP is HP - Damage,
+    retract(player(Role, Lvl, Exp, Attack, Defense, MaxHP, HP, Hearts, Gold)),
+    asserta(player(Role, Lvl, Exp, Attack, Defense, MaxHP, NewHP, Hearts, Gold)), !.
 
 checkEnemySpecialAttack :-
     enemySpecialAttack(X),
@@ -79,25 +82,10 @@ enemyMove(1) :-
     C is DefensePlayer - 2,
     D is DefensePlayer + 3, random(C, D, Defense),
     Damage is Attack - Defense,
-    Damage < 0, Damage is 0,
-    checkEnemySpecialAttack, 
+    playerDamaged(Damage, ActualDamage),
+    checkEnemySpecialAttack,
     format('~w performed attack\n', [Type]),
-    format('~w deal ~d damage\n', [Type, Damage]), 
-    playerDamaged(Damage),!.
-
-enemyMove(1) :-
-    player(_, _, _, _, DefensePlayer, _, _, _, _),
-    enemy(Type, _, _, AttackEnemy, _),
-    A is AttackEnemy - 5,
-    B is AttackEnemy + 6, random(A, B, Attack),
-    C is DefensePlayer - 2,
-    D is DefensePlayer + 3, random(C, D, Defense),
-    Damage is Attack - Defense,
-    Damage >= 0,
-    checkEnemySpecialAttack, 
-    format('~w performed attack\n', [Type]),
-    format('~w deal ~d damage\n', [Type, Damage]), 
-    playerDamaged(Damage),!.
+    format('~w deal ~d damage\n', [Type, ActualDamage]), checkPlayerDefeated(X), !.
 
 /* special attack */
 enemyMove(2) :-
@@ -106,57 +94,51 @@ enemyMove(2) :-
     A is 2 * AttackEnemy - 5, B is 2 * AttackEnemy + 6, random(A, B, Attack),
     C is DefensePlayer - 2, D is DefensePlayer + 3, random(C, D, Defense),
     Damage is Attack - Defense,
-    Damage >= 0,
+    playerDamaged(Damage, ActualDamage),
     format('~w performed special attack\n', [Type]),
     format('~w deal ~d damage\n', [Type, Damage]), 
-    playerDamaged(Damage), 
     retract(enemySpecialAttack(_)),
-    asserta(enemySpecialAttack(2)), !.
+    asserta(enemySpecialAttack(2)), checkPlayerDefeated(X),!.
 
-enemyMove(2) :-
-    player(_, _, _, _, DefensePlayer, _, _, _, _),
-    enemy(Type, _, _, AttackEnemy, _),
-    A is 2 * AttackEnemy - 5, B is 2 * AttackEnemy + 6, random(A, B, Attack),
-    C is DefensePlayer - 2, D is DefensePlayer + 3, random(C, D, Defense),
-    Damage is Attack - Defense,
-    Damage =< 0, Damage is 0,
-    format('~w performed special attack\n', [Type]),
-    format('~w deal ~d damage\n', [Type, Damage]), 
-    playerDamaged(Damage), 
-    retract(enemySpecialAttack(_)),
-    asserta(enemySpecialAttack(2)), !.
+ checkEnemyDefeated(1) :-
+    enemy(Type, LevelEnemy, HPEnemy, AttackEnemy, DefenseEnemy),
+    HPEnemy > 0, !.
 
-% kayaknya ada masalah disini tapi gatau kenapa
+ checkEnemyDefeated(0) :-
+    enemy(Type, LevelEnemy, HPEnemy, AttackEnemy, DefenseEnemy),
+    HPEnemy =< 0,
+    format('You defeated ~w!', [Type]),
+    retract(enemy(Type, LevelEnemy, HPEnemy, AttackEnemy, DefenseEnemy)),
+    retract(inBattle(1)), asserta(inBattle(0)), 
+    retract(specialAttackCount(_)),
+    retract(enemySpecialAttack(_)), !.
 
 randomEnemyMove(X) :- 
+    checkEnemyDefeated(Defeated),
+    Defeated =:= 0, !.
+
+randomEnemyMove(X) :- 
+    checkEnemyDefeated(Defeated),
+    Defeated =:= 1,
     X =:= 0,
     random(1,3,Move),
     enemyMove(Move),!.
 
 randomEnemyMove(X) :-
+    checkEnemyDefeated(Defeated),
+    Defeated =:= 1,
     X > 0,
     enemyMove(1),!.
 
-enemyDamaged(Damage) :- Damage < 1, !.
+enemyDamaged(Damage, ActualDamage) :- Damage < 1, ActualDamage is 0, !.
 
-enemyDamaged(Damage) :-
+enemyDamaged(Damage, ActualDamage) :-
     Damage > 0,
+    ActualDamage is Damage,
     enemy(Type, LevelEnemy, HPEnemy, AttackEnemy, DefenseEnemy),
     NewHP is HPEnemy - Damage,
-    NewHP > 0,
     retract(enemy(Type, LevelEnemy, HPEnemy, AttackEnemy, DefenseEnemy)),
     asserta(enemy(Type, LevelEnemy, NewHP, AttackEnemy, DefenseEnemy)).
-
-enemyDamaged(Damage) :-
-    Damage > 0,
-    enemy(Type, LevelEnemy, HPEnemy, AttackEnemy, DefenseEnemy),
-    NewHP is HPEnemy - Damage,
-    NewHP < 1,
-    retract(enemy(Type, LevelEnemy, HPEnemy, AttackEnemy, DefenseEnemy)),
-    retract(inBattle(1)), asserta(inBattle(0)), 
-    retract(specialAttackCount(_)),
-    retract(enemySpecialAttack(_)), 
-    format('You defeated a ~w!', [Type]), !.
 
 checkSpecialAttack :-
     specialAttackCount(X),
@@ -180,21 +162,11 @@ attack :-
     A is AttackPlayer - 5, B is AttackPlayer + 6, random(A, B, Attack),
     C is DefenseEnemy - 2, D is DefenseEnemy + 3, random(C, D, Defense),
     Damage is Attack - Defense, 
-    Damage >= 0,
-    format('You deal ~d damage\n', [Damage]), 
-    enemyDamaged(Damage), checkSpecialAttack,
-    enemySpecialAttack(X), randomEnemyMove(X), !.
-
-attack :-
-    inBattle(1),
-    player(_, _, _, AttackPlayer, _, _, _, _, _),
-    enemy(_, _, _, _, DefenseEnemy),
-    A is AttackPlayer - 5, B is AttackPlayer + 6, random(A, B, Attack),
-    C is DefenseEnemy - 2, D is DefenseEnemy + 3, random(C, D, Defense),
-    Damage is Attack - Defense,
-    Damage < 0, Damage is 0,
-    format('You deal ~d damage\n', [Damage]), 
-    enemyDamaged(Damage), checkSpecialAttack, 
+    enemyDamaged(Damage, ActualDamage), 
+    format('You deal ~d damage\n', [ActualDamage]), 
+    retract(chancetorun(_)), 
+    asserta(chancetorun(0)),
+    checkSpecialAttack,
     enemySpecialAttack(X), randomEnemyMove(X), !.
 
 specialattack :-
@@ -216,42 +188,36 @@ specialattack :-
     A is 2 * AttackPlayer - 5, B is 2 * AttackPlayer + 6, random(A, B, Attack),
     C is DefenseEnemy - 2, D is DefenseEnemy + 3, random(C, D, Defense),
     Damage is Attack - Defense,
-    Damage < 0, Damage is 0,
-    format('You deal ~d damage\n', [Damage]), 
-    enemyDamaged(Damage),
+    enemyDamaged(Damage, ActualDamage),
+    format('You deal ~d damage\n', [ActualDamage]), 
     retract(specialAttackCount(_)),
     asserta(specialAttackCount(2)),
-    enemySpecialAttack(X), randomEnemyMove(X), !.
-
-specialattack :-
-    inBattle(1),
-    specialAttackCount(X),
-    X =:= 0,
-    player(_, _, _, AttackPlayer, _, _, _, _, _),
-    enemy(_, _, _, _, DefenseEnemy),
-    A is 2 * AttackPlayer - 5, B is 2 * AttackPlayer + 6, random(A, B, Attack),
-    C is DefenseEnemy - 2, D is DefenseEnemy + 3, random(C, D, Defense),
-    Damage is Attack - Defense,
-    Damage >= 0,
-    format('You deal ~d damage\n', [Damage]),
-    enemyDamaged(Damage), 
-    retract(specialAttackCount(_)),
-    asserta(specialAttackCount(2)),
+    retract(chancetorun(_)), 
+    asserta(chancetorun(1)),
     enemySpecialAttack(X), randomEnemyMove(X), !.
 
 run :-
+    chancetorun(0),
+    write('You failed to run!\nChoose another move!'), !.
+
+run :-
+    chancetorun(1),
     random(1,3,X),
     runSucceed(X), !.
 
 runSucceed(1) :- 
     write('You managed to run!'), 
     retractall(enemy(_,_,_,_,_)), 
+    retract(chancetorun(_),
     retract(inBattle(1)),
     asserta(inBattle(0)), 
     retract(specialAttackCount(_)),
     retract(enemySpecialAttack(_)), !.
 
-runSucceed(2) :- write('You failed to run!\nChoose another move!'), !.
+runSucceed(2) :- 
+    write('You failed to run!\nChoose another move!'), 
+    retract(chancetorun(_)), 
+    asserta(chancetorun(0)),!.
 
 randomLevel(LvlPlayer, LevelEnemy) :-
     LvlPlayer =:= 1,
@@ -268,49 +234,52 @@ randomLevel(LvlPlayer, LevelEnemy) :-
     random(A, B, LevelEnemy), !.
 
 /* enemy(JenisEnemy, LevelEnemy, HPEnemy, AttackEnemy, DefenseEnemy)*/
-initEnemy(5) :-
+initEnemy(3) :-
     write('Your boss gave you a milestone to finish!'),
     player(_, Level, _, _, _, _, _, _, _),
     randomLevel(Level, LevelEnemy),
-    HPEnemy is (LevelEnemy - 1) * 20 + 50,
+    HPEnemy is (LevelEnemy - 1) * 15 + 60,
     AttackEnemy is (LevelEnemy - 1) * 2 + 10,
     DefenseEnemy is (LevelEnemy - 1) * 2 + 10,
     asserta(enemy('Milestone', LevelEnemy, HPEnemy, AttackEnemy, DefenseEnemy)),
     asserta(specialAttackCount(0)),
     asserta(enemySpecialAttack(0)), 
+    asserta(chancetorun(1)),
     retract(inBattle(0)),
     asserta(inBattle(1)), !.
 
-initEnemy(10) :-
+initEnemy(6) :-
     write('There is a maintenance you need to do!'), 
     player(_, Level, _, _, _, _, _, _, _),
     randomLevel(Level, LevelEnemy),
-    HPEnemy is (LevelEnemy - 1) * 10 + 50,
+    HPEnemy is (LevelEnemy - 1) * 10 + 60,
     AttackEnemy is (LevelEnemy - 1) * 4 + 10,
     DefenseEnemy is (LevelEnemy - 1) * 2 + 10,
     asserta(enemy('Maintenance', LevelEnemy, HPEnemy, AttackEnemy, DefenseEnemy)), 
     asserta(enemySpecialAttack(0)),
     asserta(specialAttackCount(0)),
+    asserta(chancetorun(1)),
     retract(inBattle(0)),
     asserta(inBattle(1)), !.
 
-initEnemy(15) :-
+initEnemy(9) :-
     write('A data breach has happened!'),
     player(_, Level, _, _, _, _, _, _, _),
     randomLevel(Level, LevelEnemy),
-    HPEnemy is (LevelEnemy - 1) * 10 + 50,
+    HPEnemy is (LevelEnemy - 1) * 10 + 60,
     AttackEnemy is (LevelEnemy - 1) * 2 + 10,
     DefenseEnemy is (LevelEnemy - 1) * 4 + 10,
     asserta(enemy('Data Breach', LevelEnemy, HPEnemy, AttackEnemy, DefenseEnemy)), 
     asserta(enemySpecialAttack(0)),
     asserta(specialAttackCount(0)),
+    asserta(chancetorun(1)),
     retract(inBattle(0)),
     asserta(inBattle(1)), !.
 
-/* initEnemy(X) :- X \= 5, X \= 10, X \= 15, !. */
+initEnemy(X) :- X \= 3, X \= 6, X \= 9, !.
 
 randomEnemy :-
-    random(0,20,X),
+    random(0,10,X),
     initEnemy(X), !.
 
 /*pas attack, kalo HP enemynya udah 0, berarti bisa dikalahkan.
