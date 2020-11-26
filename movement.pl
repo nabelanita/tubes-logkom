2bcde118ac671e5fd5bbcfb6dd6815bb4450ab68
nabelanita <utami.nabelanita@gmail.com>
2020-11-26 20:14:38 +0700

    bug teleport

--- movement.pl
+++ movement.pl
@@ -348,7 +348,7 @@ enterGateUpper(yes) :-
     gateLower(X1,Y1),
     retract(playerPos(X,Y)),
     X2 is X1,
-    Y2 is Y1 - 1,
+    Y2 is Y1 + 1,
     asserta(playerPos(X2,Y2)),
     write('You have been teleported!\n'),!.
 enterGateUpper(_) :- !.
